7cbfa1454cc9891c8fa8e8db62ed96ca71297273
tomito26 <tommybwah@gmail.com>
2021-09-30 05:53:19 +0300

    added a background image and different background-colors for the header of the different activitie

--- src/components/Duration.js
+++ src/components/Duration.js
@@ -1,18 +1,27 @@
-import ellipsis from '../images/icon-ellipsis.svg'
+import ellipsis from '../images/icon-ellipsis.svg';
+import exercise from '../images/icon-exercise.svg';
+import play from '../images/icon-play.svg';
+import selfcare from '../images/icon-self-care.svg';
+import social from '../images/icon-social.svg';
+import study from '../images/icon-study.svg';
+import work from '../images/icon-work.svg';
 
 const Duration = ({ timeDuration }) =>{
-    console.log(timeDuration.timeframes)
+    
+
     return(
-    <div className="card">
-        <div className="activity-header">
-            <h3>{timeDuration.title}</h3>
-            <img src={ellipsis} alt="ellipsis" />
+        <div className="wrapper">
+            <div className="card-body">
+                <div className="activity-header">
+                    <h3>{timeDuration.title}</h3>
+                    <img src={ellipsis} alt="ellipsis" />
+                </div>
+                <div className="activity-body">
+                    <p className="current">{timeDuration.timeframes.weekly.current}hrs</p>
+                    <p className="previous">Last Week - {timeDuration.timeframes.weekly.previous}hrs</p>
+                </div>
+            </div>
         </div>
-        <div className="activity-body">
-            <p className="current">{timeDuration.timeframes.weekly.current}hrs</p>
-            <p className="previous">Last Week{timeDuration.timeframes.weekly.previous}hrs</p>
-        </div>
-    </div>
     );
 };
 export default Duration;
